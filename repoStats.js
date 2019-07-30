e4b8e96f45d2cbefaf71a51f752f3eaef53a7807
redallen <zallen@redhat.com>
2019-07-30 11:40:09 -0400

    Add new stats and yargs script

--- repoStats.js
+++ repoStats.js
@@ -1,6 +1,10 @@
-const { execSync } = require("child_process");
-const fs = require('fs');
+const { execSync } = require('child_process');
+const fs = require('fs-extra');
+const glob = require('glob');
 const { getLocalRepoStats } = require('./getLocalRepoStats');
+const AWS = require('aws-sdk');
+AWS.config.update({region:'us-east-1'});
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 const repos = [
   {
@@ -91,23 +95,61 @@ if (!fs.existsSync('stats')) {
   fs.mkdirSync('stats');
 }
 
-repos
-  .filter(repo => process.argv[2] === '-p' || !repo.private)
-  .forEach(repo => {
-    const repoName = repo.git.split('/').pop();
-    const tmpPath = `./tmp/${repoName}`;
-    if (!fs.existsSync(tmpPath)) {
-      execSync(`git clone "${repo.git}" "${tmpPath}" --depth 1`);
-    }
-    const results = getLocalRepoStats(`./tmp/${repoName}`);
-    results.repo = repo.git;
-    results.name = repo.name || repoName;
-    results.date = new Date().toISOString();
+require('yargs')
+  .scriptName('repoStats')
+  .usage('$0 ...flags')
+  .command('collect', 'save stats locally', yargs => {
+    yargs.option('p', {
+      type: 'boolean',
+      default: 'false',
+      describe: 'whether to clone private repos'
+    })
+  }, argv => {
+    const date = new Date().toISOString();
+    const dir = `stats/${date.substr(0, 10)}`;
+    fs.removeSync('./tmp');
+    repos
+      .filter(repo => argv.p || !repo.private) // Only public repos unless flag passed
+      .forEach(repo => {
+        const repoName = repo.git.split('/').pop();
+        const tmpPath = `./tmp/${repoName}`;
+        execSync(`git clone "${repo.git}" "${tmpPath}" --depth 1`);
+        const results = getLocalRepoStats(`./tmp/${repoName}`);
+        results.repo = repo.git;
+        results.name = repo.name || repoName;
+        results.date = date;
 
-    const dir = `stats/${results.date.substr(0, 10)}`;
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    fs.writeFileSync(`${dir}/${repo.name}.json`, JSON.stringify(results, null, 2));
-  });
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir);
+        }
+        fs.writeFileSync(`${dir}/${repo.name}.json`, JSON.stringify(results, null, 2));
+      });
+    console.log(`Collected stats for ${date} under ${dir}`);
+  })
+  .command('upload [folder]', 'upload stats to DynamoDB', yargs => {
+    yargs.positional('folder', {
+      type: 'string',
+      describe: 'folder of stats to upload'
+    })
+  }, argv => {
+    if (!argv.folder)
+      throw new Error('Must specify folder under ./stats to upload');
 
+    glob.sync(`./stats/${argv.folder}/*.json`).forEach(statFile => {
+      console.log('uploading', statFile);
+      const stat = require(statFile);
+      const params = {
+        TableName: 'repo-analytics',
+        Item: {
+          repoDate: `${stat.date}${stat.repo}`,
+          ...stat
+        }
+      };
+
+      docClient.put(params, (err, data) => {
+        if (err) console.log(err);
+      });
+    });
+  })
+  .help()
+  .argv
