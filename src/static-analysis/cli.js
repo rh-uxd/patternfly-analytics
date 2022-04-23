b5d3d4943a86338d432de59528e7e83d94e6b0ae
evwilkin <evan.wilkinson@gmail.com>
2022-04-22 21:11:45 -0400

    added _all_sorted output which groups imports by type & orders by quantity

--- src/static-analysis/cli.js
+++ src/static-analysis/cli.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const { execSync } = require('child_process');
 const fs = require('fs-extra');
+// IMPORT FUNCTIONS FROM THIS DIRECTORY
 const { getPatternflyStats, patternflyAggs } = require('./getPatternfyStats');
 const { getPackageStats, getAggregatePackageStats } = require('./getPackageStats');
+const { getSortedImports } = require('./getSortedImports');
+// IMPORT JSON LIST OF REPOS
 const repos = require('../../repos.json').repos;
 
+// DEFINE OUTPUT DIRECTORIES
 const statsDir = path.resolve(__dirname, '../../stats-static');
 const tmpDir = path.resolve(__dirname, '../../tmp');
 
@@ -14,10 +18,14 @@ if (!fs.existsSync(statsDir)) {
 
 function collectPatternflyStats(argv) {
   const date = new Date().toISOString();
+  // CREATE NEW DIRECTORY W/TODAY'S DATE FOR REPORT
+  // STATS-STATIC/{DATE}
   const dir = `${statsDir}/${date.substr(0, 10)}`;
   if (argv.c) {
     fs.removeSync(tmpDir);
   }
+  // LOOP THROUGH EVERY REPO & CLONE INTO NEW DIRECTORY (IF NOT ALREADY EXISTS)
+  // OR GIT PULL (IF DOES EXIST)
   repos
     .filter(repo => argv.p || !repo.private) // Only public repos unless flag passed
     .forEach(repo => {
@@ -42,6 +50,7 @@ function collectPatternflyStats(argv) {
   if (argv.j) {
     fs.outputFileSync(`${dir}/_all_dependencies.json`, JSON.stringify(getAggregatePackageStats(), null, 2));
     fs.outputFileSync(`${dir}/_all.json`, JSON.stringify(patternflyAggs, null, 2));
+    fs.outputFileSync(`${dir}/_all_sorted.json`, JSON.stringify(getSortedImports(patternflyAggs.imports), null, 2));
   }
   console.log(`Collected stats for ${date} under ${dir}`);
 }
---
+++ src/static-analysis/getSortedImports.js
@@ -0,0 +1,61 @@
+const sortArr = arr => arr.sort((a,b) => b[1] - a[1]);
+const arrToObj = arr => arr.reduce((acc, cur) => {
+  acc[cur[0]] = cur[1];
+  return acc;
+}, {});
+
+const getImportCategory = importPath => {
+  const categoriesRegex = /\b(?:icons|utilities|layouts|charts|extensions|quickstarts|react-styles|react-tokens)\b/gi;
+  const matches = importPath.match(categoriesRegex);
+  const category = matches ? matches[0] : 'components';
+  return category;
+}
+
+const sortImportsByCount = importsObj => {
+  const entriesArr = Object.entries(importsObj);
+  const sortedEntriesArr = sortArr(entriesArr);
+  const sortedEntriesObj = arrToObj(sortedEntriesArr);
+  return sortedEntriesObj;
+}
+
+// Build object with imports grouped by category
+const buildCategorizedImports = obj => {
+  let categorizedImports = {};
+  const importsArr = Object.entries(obj);
+  importsArr.map(([importPath, importedItemsObj]) => {
+    // get category based on import path & add field to categorizedImports if needed
+    const category = getImportCategory(importPath);
+    if (!categorizedImports[category]) {
+      categorizedImports[category] = {};
+    }
+    // loop through importedItems & add to categorizedImports
+    const importedItemsArr = Object.entries(importedItemsObj);
+    importedItemsArr.map(([importName, importCount]) => {
+      categorizedImports[category][importName] = categorizedImports[category][importName]
+        ? categorizedImports[category][importName] += importCount
+        : importCount;
+    });
+  });
+  return categorizedImports;
+}
+
+// Sort categorized imports by quantity
+const sortCategorizedImports = categorizedImportsObj => {
+  let sortedCategorizedImports = {};
+  const categorizedImportsArr = Object.entries(categorizedImportsObj);
+  categorizedImportsArr.map(([category, importedItemsObj]) => {
+    const sortedCategory = sortImportsByCount(importedItemsObj);
+    sortedCategorizedImports[category] = sortedCategory;
+  });
+  return sortedCategorizedImports;
+}
+
+const getSortedImports = (importsObj) => {
+  const importCategories = buildCategorizedImports(importsObj);
+  const sortedCategories = sortCategorizedImports(importCategories);
+  return sortedCategories;
+}
+
+module.exports = {
+  getSortedImports
+}
