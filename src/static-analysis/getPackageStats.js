2ab131543959c70e5002ffb04030454c01c34220
Jan Wright <jawright@redhat.com>
2023-03-22 16:47:44 -0400

    removed YOYO comment, count peerDependencies, udates to tracked repos

--- src/static-analysis/getPackageStats.js
+++ src/static-analysis/getPackageStats.js
@@ -120,7 +120,7 @@ function getPackageStats(repoPath, repoName) {
         //write csv file of dependency for each package.json found
         const date = new Date().toISOString();
         const statsDir = path.resolve(__dirname, '../../stats-static');
-       // console.log(`%%%%%%%YOYOYOY ${statsDir}/${date.substr(0, 10)}/${repoName}-${extractFilename(filePath)}`);
+    
         fsj.writeFileSync(`${statsDir}/${date.substr(0, 10)}/${repoName}-${extractFilename(filePath)}-data.csv`, data, "utf-8", (err) => {
           if (err) console.log(err);
           else console.log("Data saved");
@@ -135,6 +135,7 @@ function getPackageStats(repoPath, repoName) {
       outputDeps(packageJSON.peerDependencies, file);
       countDeps(packageJSON.dependencies);
       countDeps(packageJSON.devDependencies);
+      countDeps(packageJSON.peerDependencies);
     } catch(error) {
       console.error(`Problem parsing JSON file ${file}:\n${error}`);
     }
