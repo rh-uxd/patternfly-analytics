2cc10c97261ff97bbe0e2946ef33e97e435a960f
Evan <evan.wilkinson@redhat.com>
2023-03-23 15:15:23 -0400

    add counts by component & repo

--- src/static-analysis/getPatternflyStats.js
+++ src/static-analysis/getPatternflyStats.js
@@ -75,12 +75,23 @@ function getPatternflyStats(repoPath, repoName) {
       patternflyAggs.classes[className] = patternflyAggs.classes[className] || 0;
       patternflyAggs.classes[className]++;
       // update productUsage
-      productUsage.classes[className] = productUsage.classes[className] || {};
-      productUsage.classes[className][repoName] = productUsage.classes[className][repoName] || [];
-      productUsage.classes[className][repoName].push(fileName);
+      productUsage.classes[className] = productUsage.classes[className] || {
+        "product_count": 0,
+        "total_usage": 0
+      };
+      const classDataObj = productUsage.classes[className];
+      if (!classDataObj[repoName]) {
+        classDataObj[repoName] = { "repo_usage": 0 };
+        classDataObj.product_count++;
+      }
+      classDataObj[repoName][fileName] = classDataObj[repoName][fileName] || 0;
+      classDataObj[repoName][fileName]++;
+      classDataObj[repoName].repo_usage++;
+      classDataObj.total_usage++;
     }
   }
 
+  // Check for /deprecated && /next, update component name to avoid counting as current component
   // Build { result: { imports } }
   jsFiles.forEach(file => {
     const stat = fs.lstatSync(file);
@@ -105,17 +116,37 @@ function getPatternflyStats(repoPath, repoName) {
         .forEach(imp => {
           const pkgName = regMatch[2];
           let pkg = result.imports[pkgName];
+          // update component name if deprecated or next component
+          if (pkgName.includes('/deprecated')) {
+            imp = `${imp}-deprecated`;
+          } else if (pkgName.includes('/next')) {
+            imp = `${imp}-next`;
+          }
           pkg[imp] = pkg[imp] || 0;
           pkg[imp]++;
           pkg = patternflyAggs.imports[pkgName];
           pkg[imp] = pkg[imp] || 0;
           pkg[imp]++;
           // product usage
-          productUsage.imports[pkgName] = productUsage.imports[pkgName] || {};
-          pkg = productUsage.imports[pkgName];
-          pkg[imp] = pkg[imp] || {};
-          pkg[imp][repoName] = pkg[imp][repoName] || [];
-          pkg[imp][repoName].push(fileName);
+          productUsage.imports[imp] = productUsage.imports[imp] || { "product_count": 0, "total_usage": 0 };
+          let component = productUsage.imports[imp]; // Card
+          // Increment product count if repo not yet tracked
+          if (!component[repoName]) {
+            component.product_count++;
+            component[repoName] = {
+              "unique_import_paths": 0,
+              "repo_usage": 0
+            };
+          }
+          // Increment package import count if new import path
+          if (!component[repoName][pkgName]) {
+            component[repoName][pkgName] = {};
+            component[repoName].unique_import_paths++;
+          }
+          component[repoName][pkgName][fileName] = component[repoName][pkgName][fileName] || 0;
+          component[repoName][pkgName][fileName]++;
+          component[repoName].repo_usage++;
+          component.total_usage++;
         });
     }
 
@@ -137,17 +168,30 @@ function getPatternflyStats(repoPath, repoName) {
         fileName = file.replace(repoPath, '')
         result.files.withPatternfly[ext][fileName] = true;
       }
+      // Variable to report on
       const cssVarName = regMatch[1];
+      // Track for repo's json
       result.cssVars[cssVarName] = result.cssVars[cssVarName] || 0;
       result.cssVars[cssVarName]++;
+      // Track for _all.json && _all_sorted.json reports
       patternflyAggs.cssVars[cssVarName] = patternflyAggs.cssVars[cssVarName] || 0;
       patternflyAggs.cssVars[cssVarName]++;
-      // update productUsage
-      productUsage.cssVars[cssVarName] = productUsage.cssVars[cssVarName] || {};
-      productUsage.cssVars[cssVarName][repoName] = productUsage.cssVars[cssVarName][repoName] || [];
-      if (!productUsage.cssVars[cssVarName][repoName].includes(fileName)) {
-        productUsage.cssVars[cssVarName][repoName].push(fileName);
+      // Track for _all_product_uses.json report
+      productUsage.cssVars[cssVarName] = productUsage.cssVars[cssVarName] || {
+        "product_count": 0,
+        "total_usage": 0
+      };
+      let varDataObj = productUsage.cssVars[cssVarName];
+      if (!varDataObj[repoName]) {
+        varDataObj[repoName] = { "repo_usage": 0 };
+        // Tally total number of unique products (by repoName)
+        varDataObj.product_count++;
       }
+      // Track which files, and how many times, where cssVarName is used per repo
+      varDataObj[repoName][fileName] = varDataObj[repoName][fileName] || 0;
+      varDataObj[repoName][fileName]++;
+      varDataObj[repoName].repo_usage++;
+      varDataObj.total_usage++;
     }
 
     matchClasses(contents, file);
